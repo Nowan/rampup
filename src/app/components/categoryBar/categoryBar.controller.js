75102d0721b1f06f14976263869181e04ab60f9b
dnipromv <dnipromv@gmail.com>
2017-10-19 14:30:56 +0200

    fill category bar with categories

--- src/app/components/categoryBar/categoryBar.controller.js
+++ src/app/components/categoryBar/categoryBar.controller.js
@@ -9,6 +9,22 @@
   function CategoryBarController() {
     var vm = this;
 
+    vm.selectedIndex = 0;
+    vm.categories = [
+      "Data Structures",
+      "Sorting",
+      "Traversing",
+      "Common Operations",
+      "Design Patterns",
+      "Excercises"
+    ];
+
+    vm.selectCategory = selectCategory;
+
+    function selectCategory(categoryIndex) {
+      if (vm.selectedIndex === categoryIndex) return;
+      vm.selectedIndex = categoryIndex;
+    }
   }
 
 })();
